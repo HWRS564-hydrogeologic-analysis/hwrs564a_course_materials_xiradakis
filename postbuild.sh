fc15a42462b7500bf26af5ce8d6da0dd61e2a8db
arbennett <bennett.andr@gmail.com>
2025-08-07 16:19:16 -0700

    Move to postbuild.sh

---
+++ postbuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+uv sync
+source ./venv/bin/activate
+mkdir -p ./modflow
+python -c "from flopy.utils import get_modflow; get_modflow('./modflow')"
+python -m ipykernel install --user --name hwrs564a
