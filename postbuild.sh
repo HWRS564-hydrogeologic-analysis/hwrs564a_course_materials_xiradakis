ae4293af0a1357a3635c0d4928ae9c44d301b642
Andrew Bennett <bennett.andr@gmail.com>
2025-09-11 19:27:10 -0700

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -2,8 +2,9 @@
 set -e
 cd hwrs564a_course_materials
 uv sync
-source ./.venv/bin/activate
+mv .venv ..
 cd ..
 mkdir -p ./modflow
+source ./.venv/bin/activate
 python -c "from flopy.utils import get_modflow; get_modflow('./modflow')"
 python -m ipykernel install --user --name hwrs564a
