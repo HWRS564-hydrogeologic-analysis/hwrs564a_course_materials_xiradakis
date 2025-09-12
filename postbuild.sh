f9b47ce926bf597fd1d69638c002d2c1f5fab7a9
Andrew Bennett <bennett.andr@gmail.com>
2025-09-11 20:20:33 -0700

    Refactor postbuild.sh for virtual environment setup
    Updated postbuild.sh to use 'uv venv' instead of moving .venv and adjusted directory structure.

--- postbuild.sh
+++ postbuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 cd hwrs564a_course_materials
-mv .venv ..
-cd ..
+uv venv
 uv sync
-mkdir -p ./modflow
 source ./.venv/bin/activate
+cd ..
+mkdir -p ./modflow
 python -c "from flopy.utils import get_modflow; get_modflow('./modflow')"
 python -m ipykernel install --user --name hwrs564a
